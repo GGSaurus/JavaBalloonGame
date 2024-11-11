c582ba53b9031a818747d45c28ac1855319c94a4
GGSaurus <110258530+GGSaurus@users.noreply.github.com>
2024-11-11 14:44:48 -0600

    Added Customer and swapping balloons.

--- core/src/main/java/org/latinschool/Customer.java
+++ core/src/main/java/org/latinschool/Customer.java
@@ -3,14 +3,21 @@ package org.latinschool;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
-public class Player {
-    int x,y,size;
-    public Player(int x, int y, int size) {
+import java.util.ArrayList;
+
+public class Customer {
+    int x,y,size,length;
+    ArrayList<Balloon> goal;
+    public Customer(int x, int y, int size) {
         this.x = x;
         this.y = y;
         this.size = size;
+        length = 3;
+        goal = new ArrayList<Balloon>();
+        for (int i = 0; i < length; i++) {
+            goal.add(new Balloon(x-20+20*i,y+50+30*i*(2-(i%3)),15,"" +(i+1)));
+        }
     }
-
     public void draw(ShapeRenderer shape) {
         shape.circle(x,y,size);
         shape.line(x,y-size,x,y-(5*size));
--- core/src/main/java/org/latinschool/Main.java
+++ core/src/main/java/org/latinschool/Main.java
@@ -22,27 +22,29 @@ public class Main extends ApplicationAdapter {
     ShapeRenderer shape;
     Player player;
     ArrayList<Balloon> balloons;
-    ArrayList<Balloon> goalBalloons;
     BitmapFont font;
     String s;
     int length;
+    Customer customer;
+    ArrayList<Customer> customers;
 
     @Override
     public void create() {
         length = 3;
         s = "";
-        font = new BitmapFont(Gdx.files.internal("ihnDehndwIxFP3fS_c1mSbr4.ttf.fnt"));
+        font = new BitmapFont(Gdx.files.internal("WUADS_HXd50qla1XGisjIeiq.TTF.fnt"));
         shape = new ShapeRenderer();
         batch = new SpriteBatch();
         player = new Player(130, 150, 20);
-        balloons = new ArrayList<Balloon>();
-        goalBalloons = new ArrayList<Balloon>();
+        balloons = new ArrayList<>();
+        customers = new ArrayList<>();
+        customer = new Customer(350,150,20);
         for (int i = 0; i < length * 2; i++) {
             balloons.add(new Balloon(25+20*i + 100*(i/3),150+30*(i%3)*(2-(i % 3)),15,"" + (i%3+1)));
         }
-        for (int i = 0; i < length; i++) {
-            goalBalloons.add(new Balloon(400+20*i,300+30*i*(2-(i%3)),15,"" +(i+1)));
-        }
+       // for (int i = 0; i < 3; i++) {
+        //    customers.add(new Customer())
+       // }
         Gdx.gl.glClearColor(0.8f, 0.792f, 0.761f,1f);
     }
 
@@ -56,12 +58,13 @@ public class Main extends ApplicationAdapter {
         shape.setColor(0,0,0,1);
 
         player.draw(shape);
+        customer.draw(shape);
         shape.end();
         shape.begin(Filled);
         for (Balloon balloon : balloons) {
             balloon.draw(shape);
         }
-        for (Balloon balloon : goalBalloons) {
+        for (Balloon balloon : customer.goal) {
             balloon.draw(shape);
         }
         shape.setColor(.3f,0,0,1);
@@ -72,15 +75,15 @@ public class Main extends ApplicationAdapter {
         shape.rect(180,100,40,20);
         shape.end();
         batch.begin();
-        font.getData().setScale(5);
+        font.getData().setScale(1);
         font.setColor(Color.BLACK);
-        font.draw(batch,s,250,250);
-        font.getData().setScale(0.8f);
+        font.draw(batch,s,200,300);
+        font.getData().setScale(0.2f);
         for (Balloon balloon : balloons) {
-            font.draw(batch,balloon.num,balloon.x- 2*(float) balloon.size /5,balloon.y+ (float) balloon.size /2);
+            font.draw(batch,balloon.num,balloon.x-2*(float) balloon.size / 5,balloon.y + (float) balloon.size / 2);
         }
-        for (Balloon balloon : goalBalloons) {
-            font.draw(batch,balloon.num,balloon.x-2*(float) balloon.size /5, balloon.y + (float) balloon.size /2);
+        for (Balloon balloon : customer.goal) {
+            font.draw(batch,balloon.num,balloon.x-2*(float) balloon.size / 5,balloon.y + (float) balloon.size / 2);
         }
         batch.end();
     }
@@ -88,30 +91,30 @@ public class Main extends ApplicationAdapter {
     private void input() {
         if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_1)) {
             Color temp = balloons.get(0).color;
-            balloons.get(0).color = balloons.get(goalBalloons.size()).color;
-            balloons.get(goalBalloons.size()).color = temp;
+            balloons.get(0).color = balloons.get(customer.goal.size()).color;
+            balloons.get(customer.goal.size()).color = temp;
         }
         else if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_2)) {
             Color temp = balloons.get(1).color;
-            balloons.get(1).color = balloons.get(1+goalBalloons.size()).color;
-            balloons.get(1+goalBalloons.size()).color = temp;
+            balloons.get(1).color = balloons.get(1+customer.goal.size()).color;
+            balloons.get(1+customer.goal.size()).color = temp;
         }
         else if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_3)) {
             Color temp = balloons.get(2).color;
-            balloons.get(2).color = balloons.get(2+goalBalloons.size()).color;
-            balloons.get(2+goalBalloons.size()).color = temp;
+            balloons.get(2).color = balloons.get(2+customer.goal.size()).color;
+            balloons.get(2+customer.goal.size()).color = temp;
         }
         else if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
-            for (Balloon balloon : goalBalloons) {
+            for (int i =0; i < customer.goal.size(); i++) {
                 switch(random.nextInt(3)) {
                     case 0:
-                        balloon.color = Color.RED;
+                        balloons.get(i).color = Color.RED;
                         break;
                     case 1:
-                        balloon.color = Color.GREEN;
+                        balloons.get(i).color = Color.GREEN;
                         break;
                     case 2:
-                        balloon.color = Color.BLUE;
+                        balloons.get(i).color = Color.BLUE;
                         break;
                 }
             }
@@ -119,34 +122,34 @@ public class Main extends ApplicationAdapter {
         if (Gdx.input.justTouched()) {
             if (Gdx.input.getX() < 100 && Gdx.input.getX() > 20 && (Gdx.graphics.getHeight() - Gdx.input.getY()) > 30 && (Gdx.graphics.getHeight() - Gdx.input.getY()) < 80) {
                 boolean yes = true;
-                for (int i = 0; i < goalBalloons.size(); i++) {
-                    if (!(balloons.get(i).color == goalBalloons.get(i).color)) {
+                for (int i = 0; i < customer.goal.size(); i++) {
+                    if (!(balloons.get(i).color == customer.goal.get(i).color)) {
                         yes = false;
                     }
                 }
                 if (yes) {
-                    for (Balloon balloon : goalBalloons) {
-                        s = "yes";
+                    for (Balloon balloon : customer.goal) {
+                        s = "Yes";
                     }
                 }
                 else {
-                    s = "no";
+                    s = "No";
                 }
             }
             else if(Gdx.input.getX() < 240 && Gdx.input.getX() > 160 && (Gdx.graphics.getHeight() - Gdx.input.getY()) > 30 && (Gdx.graphics.getHeight() - Gdx.input.getY()) < 80) {
                 boolean yes = true;
-                for (int i = 0; i < goalBalloons.size(); i++) {
-                    if (!(balloons.get(i+goalBalloons.size()).color == goalBalloons.get(i).color)) {
+                for (int i = 0; i < customer.goal.size(); i++) {
+                    if (!(balloons.get(i+customer.goal.size()).color == customer.goal.get(i).color)) {
                         yes = false;
                     }
                 }
                 if (yes) {
-                    for (Balloon balloon : goalBalloons) {
-                        s = "yes";
+                    for (Balloon balloon : customer.goal) {
+                        s = "Yes";
                     }
                 }
                 else {
-                    s = "no";
+                    s = "No";
                 }
             }
             else if(Gdx.input.getX() < 80 && Gdx.input.getX() > 40 && (Gdx.graphics.getHeight() - Gdx.input.getY()) > 100 && (Gdx.graphics.getHeight() - Gdx.input.getY()) < 120) {
@@ -155,11 +158,11 @@ public class Main extends ApplicationAdapter {
                 balloons.get(2).color = balloons.get(1).color;
                 balloons.get(1).color = temp;
             }
-            else if(Gdx.input.getX() < 180 && Gdx.input.getX() > 140 && (Gdx.graphics.getHeight() - Gdx.input.getY()) > 100 && (Gdx.graphics.getHeight() - Gdx.input.getY()) < 120) {
-                Color temp = balloons.get(goalBalloons.size()).color;
-                balloons.get(goalBalloons.size()).color = balloons.get(2+goalBalloons.size()).color;
-                balloons.get(2+goalBalloons.size()).color = balloons.get(1+goalBalloons.size()).color;
-                balloons.get(1+goalBalloons.size()).color = temp;
+            else if(Gdx.input.getX() < 220 && Gdx.input.getX() > 180 && (Gdx.graphics.getHeight() - Gdx.input.getY()) > 100 && (Gdx.graphics.getHeight() - Gdx.input.getY()) < 120) {
+                Color temp = balloons.get(customer.goal.size()).color;
+                balloons.get(customer.goal.size()).color = balloons.get(2+customer.goal.size()).color;
+                balloons.get(2+customer.goal.size()).color = balloons.get(1+customer.goal.size()).color;
+                balloons.get(1+customer.goal.size()).color = temp;
             }
         }
     }
